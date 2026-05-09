ba214a4c0bcf5310f9c1a7c0181fb09c59513484
JeperaltaP <jeperaltap@eafit.edu.co>
2026-05-09 17:12:51 -0500

    Inventory, InventoryUI
    Inventario visible presionando la I, commit con inventario funcional

--- Assets/scripts/PickupItem.cs
+++ Assets/scripts/PickupItem.cs
@@ -5,6 +5,7 @@ public class PickupItem : MonoBehaviour, Interactable {
     [SerializeField] private ItemData itemData; // <- eso reemplazara el nombre y la información por lo que sea que tenga el objeto que pongamos (ItemName y Icon)
 
     public void Interact() {
+        Inventory.Instance.AddItem(itemData);
         Debug.Log($"Recogiste: {itemData.itemName}");
         Destroy(gameObject);
     }
