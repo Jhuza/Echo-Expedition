89bb4bb80d064025d6146542c6759fc0aa7fd0eb
Juanjo Hoyos <contactojhg18@gmail.com>
2026-05-25 22:26:21 -0500

    Added: colliders de las palmeras, rocas y buildings. Importante Se elimino todos los codigos

--- Assets/scripts/ItemData.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "NewItem", menuName = "Inventario/Item")]
-public class ItemData : ScriptableObject {
-    public string itemName;
-    public Sprite icon;
-    public string description;
-    
-}
